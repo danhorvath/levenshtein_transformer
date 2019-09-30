feature: fixed bleu calculation and improved model save

en_de_translate.py
@@ -1,16 +1,17 @@
-from datetime import datetime
 import torch
 import torch.nn as nn
 from torchtext import data, datasets
+from sacrebleu import corpus_bleu
 
-
-from functions import greedy_decode
-from train import LabelSmoothing, run_epoch
-from optimizer import NoamOpt
-from multi_gpu_loss_compute import MultiGPULossCompute
-from models import Transformer
-from data import batch_size_fn, MyIterator, rebatch
+from transformer.search import greedy_decode
+from train import  run_epoch
+from transformer.optimizer import NoamOpt
+from transformer.criteria import LabelSmoothing
+from transformer.multi_gpu_loss_compute import MultiGPULossCompute
+from transformer.model import Transformer
+from transformer.data import batch_size_fn, MyIterator, rebatch
 from validator import validate
+from utils import save_model
 
 from en_de_config import config
 
@@ -22,10 +23,9 @@ EOS_WORD = '</s>'
 BLANK_WORD = '<blank>'
 
 
-
 wandb.init(project="transformer")
 wandb.config.update(config)
- 
+
 
 def main():
     devices = list(range(torch.cuda.device_count()))
@@ -39,29 +39,30 @@ def main():
                      eos_token=EOS_WORD, pad_token=BLANK_WORD)
 
     train, val, test = datasets.WMT14.splits(exts=('.en', '.de'),
-                                                train='train.tok.clean.bpe.32000',
-                                                # train='newstest2014.tok.bpe.32000',
-                                                validation='newstest2013.tok.bpe.32000',
-                                                test='newstest2014.tok.bpe.32000',
-                                                fields=(SRC, TGT),
-                                                filter_pred=lambda x: len(vars(x)['src']) <= config['max_len'] and
-                                                                    len(vars(x)['trg']) <= config['max_len'],
-                                                root = './.data/')
+                                             train='train.tok.clean.bpe.32000',
+                                             # train='newstest2014.tok.bpe.32000',
+                                             validation='newstest2013.tok.bpe.32000',
+                                             test='newstest2014.tok.bpe.32000',
+                                             fields=(SRC, TGT),
+                                             filter_pred=lambda x: len(vars(x)['src']) <= config['max_len'] and
+                                             len(vars(x)['trg']
+                                                 ) <= config['max_len'],
+                                             root='./.data/')
     print('Train set length: ', len(train))
 
     # building shared vocabulary
-    TGT.build_vocab(train.src, train.trg, min_freq = config['min_freq'])
-    SRC.vocab=TGT.vocab
+    TGT.build_vocab(train.src, train.trg, min_freq=config['min_freq'])
+    SRC.vocab = TGT.vocab
 
     print('Source vocab length: ', len(SRC.vocab.itos))
     print('Target vocab length: ', len(TGT.vocab.itos))
     wandb.config.update({'src_vocab_length': len(SRC.vocab),
                          'target_vocab_length': len(TGT.vocab)})
 
-    pad_idx=TGT.vocab.stoi[BLANK_WORD]
+    pad_idx = TGT.vocab.stoi[BLANK_WORD]
     print('Pad index:', pad_idx)
 
-    train_iter=MyIterator(train, batch_size = config['batch_size'], device = torch.device(0), repeat = False,
+    train_iter = MyIterator(train, batch_size=config['batch_size'], device=torch.device(0), repeat=False,
                             sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=True)
 
     valid_iter = MyIterator(val, batch_size=config['batch_size'], device=torch.device(0), repeat=False,
@@ -105,11 +106,11 @@ def main():
             model.generator, criterion, devices=devices, opt=model_opt)
 
         (_,  steps) = run_epoch((rebatch(pad_idx, b) for b in train_iter),
-                            model_par,
-                            loss_calculator,
-                            steps_so_far=current_steps,
-                            batch_multiplier=config['batch_multiplier'],
-                            logging=True)
+                                model_par,
+                                loss_calculator,
+                                steps_so_far=current_steps,
+                                batch_multiplier=config['batch_multiplier'],
+                                logging=True)
 
         current_steps += steps
 
@@ -119,11 +120,11 @@ def main():
             model.generator, eval_criterion, devices=devices, opt=None)
 
         (loss, _) = run_epoch((rebatch(pad_idx, b) for b in valid_iter),
-                            model_par,
-                            loss_calculator_without_optimizer,
-                            steps_so_far=current_steps)
+                              model_par,
+                              loss_calculator_without_optimizer,
+                              steps_so_far=current_steps)
 
-        if (epoch > 5 and epoch % 2 == 1) or current_steps > config['max_step']:
+        if (epoch > 10) or current_steps > config['max_step']:
             # greedy decoding takes a while so Bleu won't be evaluated for every epoch
             print('Calculating BLEU score...')
             bleu = validate(model, valid_iter, SRC, TGT,
@@ -133,16 +134,12 @@ def main():
 
         print(f"Epoch {epoch} | Loss: {loss}")
         wandb.log({'Epoch': epoch, 'Epoch loss': loss})
-        if epoch % 5 == 0 and epoch != 0:
-            current_date = datetime.now().strftime("%b-%d-%Y_%H-%M")
-            torch.save(model.state_dict(), 'en-de__'+current_date+'.pt')
+        if epoch > 10:
+            save_model(model=model, optimizer=model_opt, loss=loss, src_field=SRC, tgt_field=TGT, updates=current_steps, epoch=epoch)
         if current_steps > config['max_step']:
             break
 
-    current_date = datetime.now().strftime("%b-%d-%Y_%H-%M")
-    file_name = 'en-de__'+current_date+'.pt'
-    torch.save(model.state_dict(), file_name)
-    print(f'Model saved as {file_name}')
+    save_model(model=model, optimizer=model_opt, loss=loss, src_field=SRC, tgt_field=TGT, updates=current_steps, epoch=epoch)
 
     test_bleu = validate(model, test_iter, SRC, TGT,
                          BOS_WORD, EOS_WORD, BLANK_WORD, config['max_len'], logging=True)

test_validate_only.py
@@ -21,19 +21,23 @@ BOS_WORD = '<s>'
 EOS_WORD = '</s>'
 BLANK_WORD = '<blank>'
 
+PATH = 'last_model_en-de__Sep-30-2019_18-19___old.pt'
+print('Testing model ' + PATH)
 
+state = torch.load(PATH)
 
-devices = list(range(torch.cuda.device_count()))
-print('Selected devices: ', devices)
 
 def tokenize_bpe(text):
     return text.split()
 
-TGT=torch.load('SRC_36893', pickle_module=dill)
-SRC=torch.load('TGT_36893', pickle_module=dill)
+SRC = data.Field(tokenize=tokenize_bpe, pad_token=BLANK_WORD)
+TGT = data.Field(tokenize=tokenize_bpe, init_token=BOS_WORD,
+                    eos_token=EOS_WORD, pad_token=BLANK_WORD)
+
+TGT=torch.load('SRC_36893.pt', pickle_module=dill)
+SRC=torch.load('TGT_36893.pt', pickle_module=dill)
 
 train, val, test = datasets.WMT14.splits(exts=('.en', '.de'),
-                                            # train='train.tok.clean.bpe.32000',
                                             train='newstest2014.tok.bpe.32000',
                                             validation='newstest2014.tok.bpe.32000',
                                             test='newstest2014.tok.bpe.32000',
@@ -42,44 +46,20 @@ train, val, test = datasets.WMT14.splits(exts=('.en', '.de'),
                                             len(vars(x)['trg']
                                                 ) <= config['max_len'],
                                             root='./.data/')
-print('Train set length: ', len(train))
 
-
-
-print('Source vocab length: ', len(SRC.vocab.itos))
-print('Target vocab length: ', len(TGT.vocab.itos))
+# building shared vocabulary
 
 test_iter = MyIterator(test, batch_size=config['batch_size'], device=torch.device(0), repeat=False,
                         sort_key=lambda x: (len(x.src), len(x.trg)), batch_size_fn=batch_size_fn, train=False)
 
 
 model = Transformer(len(SRC.vocab), len(TGT.vocab), N=config['num_layers'])
-
-
-model.load_state_dict(torch.load('en-de__Sep-30-2019_09-19.pt'))
-print('Last model loaded.')
-save_model(model, None, loss=0, src_field=SRC, tgt_field=TGT, updates=0, epoch=0, prefix='last_model')
+model.load_state_dict(state['model_state_dict'])
+print('Model loaded.')
 
 model.cuda()
 model.eval()
 
-# test_bleu = validate(model, test_iter, SRC, TGT,
-#                         BOS_WORD, EOS_WORD, BLANK_WORD, config['max_len'], logging=True)
-# print(f"Test Bleu score: {test_bleu}")
-
-
-
-paths = ['en-de__Sep-30-2019_09-19.pt', 'en-de__Sep-29-2019_15-35.pt', 'en-de__Sep-29-2019_19-13.pt', 'en-de__Sep-29-2019_23-55.pt', 'en-de__Sep-30-2019_04-37.pt']
-average_model_state = average_checkpoints(paths)
-
-model.load_state_dict(average_model_state)
-print(f'Model loaded with with averaged parameters from {len(paths)} models.')
-save_model(model, None, loss=0, src_field=SRC, tgt_field=TGT, updates=0, epoch=0, prefix='average')
-
-model.cuda()
-model.eval()
-
-# test_bleu = validate(model, test_iter, SRC, TGT,
-#                         BOS_WORD, EOS_WORD, BLANK_WORD, config['max_len'], logging=True)
-# print(f"Test Bleu score: {test_bleu}")
-
+test_bleu = validate(model, test_iter, SRC, TGT,
+                        BOS_WORD, EOS_WORD, BLANK_WORD, config['max_len'], logging=True)
+print(f"Test Bleu score: {test_bleu}")

train.py
@@ -1,8 +1,6 @@
 import time
-
-import torch
-import torch.nn as nn
 import wandb
+from en_de_config import config
 
 
 def run_epoch(data_iter, model, loss_compute, steps_so_far, batch_multiplier=1, logging=False):
@@ -16,96 +14,30 @@ def run_epoch(data_iter, model, loss_compute, steps_so_far, batch_multiplier=1, 
 
     for i, batch in enumerate(data_iter):
         effective_step = i / batch_multiplier
-        
+
         # update the model on steps defined by batch_multiplier or the last step in the epoch
         optimizer_step = effective_step.is_integer()
 
-        out = model.forward(batch.src, batch.trg, batch.src_mask, batch.trg_mask)
+        out = model.forward(batch.src, batch.trg,
+                            batch.src_mask, batch.trg_mask)
 
         # TODO set number of batches if the number of iterations in the epoch is not dividable by batch_multiplier
-        loss = loss_compute(out, batch.trg_y, batch.ntokens, optimizer_step=optimizer_step)
+        loss = loss_compute(out, batch.trg_y, batch.ntokens,
+                            optimizer_step=optimizer_step)
 
         total_loss += loss
         total_tokens += batch.ntokens
         tokens += batch.ntokens
 
-        
         if logging and optimizer_step:
             elapsed = time.time() - start
-            wandb.log({'Step': steps_so_far + effective_step, 'Loss': loss *  batch_multiplier / batch.ntokens,
-                       'Tokens per Sec': tokens / elapsed, 'Learning rate': loss_compute.opt._rate})
+            wandb.log({'Step': steps_so_far + effective_step, 'Loss': loss * batch_multiplier / batch.ntokens,
+                       'Tokens per Sec': tokens / elapsed, 'Learning rate': loss_compute.opt._rate,
+                       'batch_length': len(batch.src), 'effective_batch_length': len(batch.src)*config['batch_multiplier']})
             if effective_step % 100 == 1:
-                print(f"Step: {steps_so_far + effective_step} | Loss: {loss * batch_multiplier / batch.ntokens} | Tokens per Sec: {tokens / elapsed} | Learning rate: {loss_compute.opt._rate}")
+                print(f"Step: {steps_so_far + effective_step} | Loss: {loss * batch_multiplier / batch.ntokens} " +
+                      f"| Tokens per Sec: {tokens / elapsed} | Learning rate: {loss_compute.opt._rate} | Batch length: {len(batch.src)}")
             start = time.time()
             tokens = 0
 
     return (total_loss / total_tokens, effective_step)
-
-
-class NoamOpt(object):
-    """
-    Optim wrapper that implements rate.
-    """
-
-    def __init__(self, model_size, factor, warmup, optimizer):
-        self.optimizer = optimizer
-        self._step = 0
-        self.warmup = warmup
-        self.factor = factor
-        self.model_size = model_size
-        self._rate = 0
-
-    def step(self):
-        """
-        Update parameters and rate
-        """
-        self._step += 1
-        rate = self.rate()
-        for p in self.optimizer.param_groups:
-            p['lr'] = rate
-        self._rate = rate
-        self.optimizer.step()
-
-    def rate(self, step=None):
-        """
-        Implement `lrate` above
-        """
-        if step is None:
-            step = self._step
-        return self.factor * (self.model_size ** -0.5) * min(step ** -0.5, step * self.warmup ** -1.5)
-
-
-def get_std_opt(model):
-    return NoamOpt(model.src_embed[0].d_model, 2, 4000,
-                   torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
-
-
-class LabelSmoothing(nn.Module):
-    """
-    Implement label smoothing. Basically this switches the original label distribution
-    (which is effectively a dirac delta) to a mixture of the original and a uniform distribution.
-    This helps the network to generalize better (and thus increase accuracy), but in turn will increase the perplexity.
-    q'(k) = (1-smoothing)*q(k) + smoothing*uniform(k) = (1-smoothing)*q(k) + smoothing * 1/output_size
-    """
-
-    def __init__(self, size, padding_idx, smoothing=0.0, batch_multiplier=1.):
-        super(LabelSmoothing, self).__init__()
-        self.criterion = nn.KLDivLoss(reduction='sum')
-        self.padding_idx = padding_idx
-        self.confidence = 1.0 - smoothing
-        self.smoothing = smoothing
-        self.size = size
-        self.true_dist = None
-        self.batch_multiplier = batch_multiplier
-
-    def forward(self, x, target):
-        assert x.size(1) == self.size
-        true_dist = x.data.clone()
-        true_dist.fill_(self.smoothing / (self.size - 2))
-        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
-        true_dist[:, self.padding_idx] = 0
-        mask = torch.nonzero(target.data == self.padding_idx)
-        if mask.dim() > 0 and mask.size(-1) > 0:
-            true_dist.index_fill_(0, mask.squeeze(), 0.0)
-        self.true_dist = true_dist
-        return self.criterion(x, true_dist.clone().detach().requires_grad_(False)) / self.batch_multiplier

transformer/data.py
@@ -1,15 +1,11 @@
-import torch
-import numpy as np
-from functions import subsequent_mask
 from torch.autograd import Variable
 from torchtext import data
-import random
+from transformer.modules import subsequent_mask
 
 from en_de_config import config
 
 max_src_in_batch = config['batch_size']
 max_tgt_in_batch = config['batch_size']
-
 
 def batch_size_fn(new, count, size_so_far):
     """
@@ -25,6 +21,14 @@ def batch_size_fn(new, count, size_so_far):
     src_elements = count * max_src_in_batch
     tgt_elements = count * max_tgt_in_batch
     return max(src_elements, tgt_elements)
+
+
+def rebatch(pad_idx, batch):
+    """
+    Fix order in torchtext to match ours
+    """
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    return Batch(src, trg, pad_idx)
 
 
 class Batch(object):
@@ -50,35 +54,6 @@ class Batch(object):
         tgt_mask = tgt_mask & Variable(
             subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
         return tgt_mask
-
-
-def data_gen(V, batch, nbatches):
-    "Generate random data for a src-tgt copy task."
-    for i in range(nbatches):
-        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
-        data[:, 0] = 1
-        src = torch.tensor(data, requires_grad=False)
-        tgt = torch.tensor(data, requires_grad=False)
-        yield Batch(src, tgt, 0)
-
-
-class SimpleLossCompute:
-    "A simple loss compute and train function."
-
-    def __init__(self, generator, criterion, opt=None):
-        self.generator = generator
-        self.criterion = criterion
-        self.opt = opt
-
-    def __call__(self, x, y, norm):
-        x = self.generator(x)
-        loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
-                              y.contiguous().view(-1)) / norm
-        loss.backward()
-        if self.opt is not None:
-            self.opt.step()
-            self.opt.optimizer.zero_grad()
-        return loss.data[0] * norm
 
 
 class MyIterator(data.Iterator):
@@ -108,11 +83,3 @@ class MyIterator(data.Iterator):
             self.batches = []
             for b in data.batch(self.data(), self.batch_size, self.batch_size_fn):
                 self.batches.append(sorted(b, key=self.sort_key))
-
-
-def rebatch(pad_idx, batch):
-    """
-    Fix order in torchtext to match ours
-    """
-    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
-    return Batch(src, trg, pad_idx)

transformer/layers.py
@@ -2,7 +2,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from functions import clones, attention
+from transformer.modules import clones, attention
 
 
 class Generator(nn.Module):
@@ -164,61 +164,6 @@ class SublayerConnection(nn.Module):
         return x + self.dropout(sublayer(self.norm(x)))
 
 
-class MultiHeadedAttention(nn.Module):
-
-    def __init__(self, h, d_model, dropout=0.1):
-        """
-        Take in model size and number of heads.
-        """
-        super(MultiHeadedAttention, self).__init__()
-        assert d_model % h == 0
-        #  We assume d_v always equals d_k
-        self.d_k = d_model // h
-        self.h = h
-        self.linears = clones(nn.Linear(d_model, d_model), 4)
-        self.attn = None
-        self.dropout = nn.Dropout(p=dropout)
-
-    def forward(self, query, key, value, mask=None):
-        """
-        Implements Figure 2
-        """
-        if mask is not None:
-            # Same mask applied to all h heads.
-            mask = mask.unsqueeze(1)
-        nbatches = query.size(0)
-        # 1) Do all the linear projections in batch from d_model => h x d_k
-        query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2) for l, x in
-                             zip(self.linears, (query, key, value))]
-        # 2) Apply attention on all the projected vectors in batch.
-        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
-        # 3) "Concat" using a view and apply a final linear.
-        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
-        return self.linears[-1](x)
-
-
-class PositionwiseFeedForward(nn.Module):
-    """Implements FFN equation."""
-
-    def __init__(self, d_model, d_ff, dropout=0.1):
-        super(PositionwiseFeedForward, self).__init__()
-        self.w_1 = nn.Linear(d_model, d_ff)
-        self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        return self.w_2(self.dropout(F.relu(self.w_1(x))))
-
-
-class Embeddings(nn.Module):
-    def __init__(self, d_model, vocab):
-        super(Embeddings, self).__init__()
-        self.lookup_table = nn.Embedding(vocab, d_model)
-        self.d_model = d_model
-
-    def forward(self, x):
-        return self.lookup_table(x) * math.sqrt(self.d_model)
-
 
 class PositionalEncoding(nn.Module):
     """Implement the PE function."""
@@ -239,3 +184,12 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.positional_encoding[:, :x.size(1)].clone().detach().requires_grad_(True)
         return self.dropout(x)
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lookup_table = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lookup_table(x) * math.sqrt(self.d_model)

transformer/model.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
 from copy import deepcopy
-from layers import MultiHeadedAttention, PositionwiseFeedForward, PositionalEncoding, EncoderDecoder, Encoder, \
-    EncoderLayer, Decoder, DecoderLayer, Embeddings, Generator
+from transformer.sublayers import MultiHeadedAttention, PositionwiseFeedForward
+from transformer.layers import  PositionalEncoding, EncoderDecoder, Encoder, \
+        EncoderLayer, Decoder, DecoderLayer, Embeddings, Generator
 
 
 def Transformer(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):

validator.py
@@ -1,69 +1,67 @@
-from nltk.translate.bleu_score import sentence_bleu
-import numpy as np
-from functions import greedy_decode
 import wandb
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu as nltk_corpus_bleu
+from sacrebleu import corpus_bleu
+import numpy as np
+from transformer.search import greedy_decode
+from utils import vector_to_sentence, get_src_mask
 
 
 def validate(model, valid_iter, SRC, TGT, BOS_WORD, EOS_WORD, BLANK_WORD, max_len, logging=False):
-    def bpe_to_words(sentence):
-        new_sentence = []
-        for i in range(len(sentence)):
-            word = sentence[i]
-            if word[-2:] == '@@' and i != len(sentence)-1:
-                sentence[i+1] = word[:-2]+sentence[i+1]
-            else:
-                new_sentence.append(word)
-        return new_sentence
+    print('Validating...')
+    # TODO: paralell Bleu calculation
 
-    def vector_to_sentence(vector, field, start_from=1):
-        sentence = []
-        for l in range(start_from, len(vector)):
-            word = field.vocab.itos[vector[l]]
-            if word == EOS_WORD:
-                break
-            sentence.append(word)
-        # return bpe_to_words(sentence)' '.join(tgt_sentences[0])
-        return bpe_to_words(sentence)
+    batch_bleus = []
+    batch_sacrebleus = []
+    hypotheses_tokenized = []
+    references_tokenized =[]
 
-    def get_weights(sentence):
-        length = len(sentence) if len(sentence) <= 4 else 4
-        return list(np.ones(length)/length)
+    hypotheses = []
+    references =[]
 
-    def get_src_mask(src):
-        return (src != SRC.vocab.stoi[BLANK_WORD]).unsqueeze(-2)
+    for i, batch in enumerate(valid_iter):
+        src = batch.src.transpose(0, 1)
+        tgt = batch.trg.transpose(0, 1)
 
-    def get_BLEU():
+        src_sentences = [vector_to_sentence(
+            src[i, :], SRC, 0) for i in range(src.size(0))]
 
-        # TODO: paralell Bleu calculation
+        tgt_sentences = [vector_to_sentence(tgt[i, :], TGT, EOS_WORD)
+                         for i in range(tgt.size(0))]
 
-        batch_bleus = []
-        for i, batch in enumerate(valid_iter):
-            src = batch.src.transpose(0, 1)
-            tgt = batch.trg.transpose(0, 1)
+        out_seqs = [greedy_decode(model, src_sentence.unsqueeze(-2), get_src_mask(src_sentence.unsqueeze(-2), SRC.vocab.stoi[BLANK_WORD]), max_len=max_len,
+                                  start_symbol=TGT.vocab.stoi[BOS_WORD], stop_symbol=TGT.vocab.stoi[EOS_WORD]) for src_sentence in src]
 
-            src_sentences = [vector_to_sentence(
-                src[i, :], SRC, 0) for i in range(src.size(0))]
+        out_sentences = [vector_to_sentence(out_seq.squeeze(), TGT, EOS_WORD)
+             for out_seq in out_seqs]
 
-            tgt_sentences = [vector_to_sentence(
-                tgt[i, :], TGT) for i in range(tgt.size(0))]
+        hypotheses_tokenized += [out_sentence.split(' ') for out_sentence in out_sentences]
+        references_tokenized += [[tgt_sentence.split(' ')] for tgt_sentence in tgt_sentences]
 
-            out_seqs = [greedy_decode(model, src_sentence.unsqueeze(-2), get_src_mask(src_sentence.unsqueeze(-2)), max_len=max_len,
-                                      start_symbol=TGT.vocab.stoi[BOS_WORD], stop_symbol=TGT.vocab.stoi[EOS_WORD]) for src_sentence in src]
+        hypotheses += out_sentences
+        references += tgt_sentences
 
-            out_sentences = [vector_to_sentence(
-                out_seq.squeeze(), TGT) for out_seq in out_seqs]
+        # sentence_pairs = list(zip(tgt_sentences, out_sentences))
+        if logging:
+            # sentence_pair = list(sentence_pairs)[0]
 
-            sentence_pairs = list(zip(tgt_sentences, out_sentences))
-            if logging:
-                sentence_pair = list(sentence_pairs)[0]
+            print(
+                f"Source: {src_sentences[0]}\nTarget: {tgt_sentences[0]}\nPrediction: {out_sentences[0]}\n")
 
-                print(f"Source: {' '.join(src_sentences[0]).encode('utf-8').decode('latin-1')}\nTarget: {' '.join(sentence_pair[0]).encode('utf-8').decode('latin-1')}\nPrediction: {' '.join(sentence_pair[1]).encode('utf-8').decode('latin-1')}\n")
+        # batch_bleu = [sentence_bleu([sentence_pair[0].split(' ')], sentence_pair[1].split(' '))
+        #                     for sentence_pair in sentence_pairs]
 
-            batch_bleu = np.array([sentence_bleu([sentence_pair[0]], sentence_pair[1], weights=(
-                0.25, 0.25, 0.25, 0.25)) for sentence_pair in sentence_pairs])
-            batch_bleus.append(batch_bleu.mean())
+        # batch_sacrebleu = [corpus_bleu(sentence_pair[1], sentence_pair[0]).score
+        #                     for sentence_pair in sentence_pairs]
 
-        bleu_score = np.array(batch_bleus).mean()
-        return bleu_score
+        # batch_bleus += batch_bleu
+        # batch_sacrebleus += batch_sacrebleu
 
-    return get_BLEU()
+        # print(f'Batch {i} | Bleu: {np.array(batch_bleu).mean() * 100} | Sacrebleu: {np.array(batch_sacrebleu).mean()}')
+
+    # bleu_score = np.array(batch_bleus).mean() * 100
+    # sacrebleu_score = np.array(batch_sacrebleus).mean()
+    # print(f'Avg bleu: {bleu_score} | Avg. sacrebleu: {sacrebleu_score}')
+    corpus_sacrebleu_score = corpus_bleu(hypotheses, [references])
+    corpus_bleu_score = nltk_corpus_bleu(references_tokenized, hypotheses_tokenized)
+    print(f'Corpus bleu: {corpus_bleu_score * 100} | Corpus sacrebleu: {corpus_sacrebleu_score.score}')
+    return corpus_sacrebleu_score.score